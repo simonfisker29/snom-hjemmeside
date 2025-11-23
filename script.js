1b84aefbe21448499df2b04e9de3444b5f798a8b
Simon Gleerup <simonfiskergleerup@gmail.com>
2025-11-23 16:36:42 +0100

    Initial commit fra lokal mappe

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+function skiftBaggrundsfarve() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+let farveknap = document.getElementById('skiftFarveKnap')
+
+document.addEventListener('click', skiftBaggrundsfarve)
